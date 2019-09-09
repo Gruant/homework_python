481188251087804f9250d2bd0413e935b2f99ddd
antongrutsin <grucinav@gmail.com>
2019-09-10 01:42:12 +0300

    Homework2_1/homework2_1.py

--- Homework2_1/homework2_1.py
+++ Homework2_1/homework2_1.py
@@ -1,10 +1,6 @@
 from pprint import pprint
-def write_dict(cook_book, dish, ingridients):
-    cook_book.setdefault(dish, [])
-    cook_book[dish].append({'ingridient_name': ingridients[0][0], 'quantity': ingridients[0][1], 'measure': ingridients[0][2]})
-    return cook_book
 
-def main():
+def print_dict():
     cook_book = dict()
     ingridients = []
     with open('recipes.txt', encoding='UTF-8') as file:
@@ -19,12 +15,42 @@ def main():
                     ingridients.append(ingridient.replace(' ', '').split('|'))
                     write_dict(cook_book, dish, ingridients)
                     ingridients = []
-    ingridients = []
-    pprint(cook_book)
+    return cook_book
+
+def write_dict(cook_book, dish, ingridients):
+    cook_book.setdefault(dish, [])
+    cook_book[dish].append({'ingridient_name': ingridients[0][0], 'quantity': ingridients[0][1], 'measure': ingridients[0][2]})
+    return cook_book
 
 
+def ingridients_list(dishes: list, person_count):
+    new_dict = dict()
+    cook_book = print_dict()
+    for dish in dishes:
+        if dish in cook_book:
+            for new in cook_book[dish]:
+              if new['ingridient_name'] in new_dict:
+                new_dict[new['ingridient_name']]['quantity'] += (int(new['quantity']) * person_count)
+              else:
+                new_dict.setdefault(new['ingridient_name'], {'quantity': (int(new['quantity']) * person_count), 'measure': new['measure']})
+        else:
+            print(f'Блюда {i} нет в списке')
+    return new_dict
+
+def main():
+    print('Добро пожаловать в домашнюю работу по к лекции 2.4 «Открытие и чтение файла, запись в файл»')
+    print('Для проверки задания №1 нажмите 1\nДля проверки задания №2 нажмите 2')
+    command = input('Ваша команда: \n')
+    if command == '1':
+        print(print_dict())
+    elif command == '2':
+        dish = map(str, input('Введите названия блюд через запятую(без пробела после запятой): ').split(','))
+        persons = int(input('Введите кол-во персон: '))
+        pprint(ingridients_list(dish, persons))
+    else:
+        pprint('Неверная команда')
+
+if __name__ == '__main__':
+    main()
 
 
-
-
-main()
