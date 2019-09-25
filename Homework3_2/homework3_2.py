d10737759db69252eca2d21c3188802665e079d2
antongrutsin <grucinav@gmail.com>
2019-09-26 01:33:58 +0300

    Init

---
+++ Homework3_2/homework3_2.py
@@ -0,0 +1,81 @@
+import requests
+
+SERVICE_TOKEN = '7abc891b7abc891b7abc891bd47ad19bff77abc7abc891b273732a14d8176e6f0aa6d55'
+
+
+class User():
+    first_name = ''
+    last_name = ''
+    # id может быть число или имя (далее мы будем всегда получать числовой id, чтобы получать список друзей.
+    id = ''
+    link = 'https://vk.com/'
+    is_closed = ''
+
+    def __init__(self, id):
+        self.id = id
+
+    def user_info(self):
+        params = {
+            'v': '5.101',
+            'access_token': SERVICE_TOKEN,
+            'user_ids': self.id,
+            'fields': 'domain'
+        }
+        response = requests.get('https://api.vk.com/method/users.get', params=params).json()
+        self.is_closed = response['response'][0]['is_closed']
+        if not self.is_closed:
+            return False
+        else:
+            self.first_name = response['response'][0]['first_name']
+            self.last_name = response['response'][0]['last_name']
+            self.id = response['response'][0]['id']
+            self.link += response['response'][0]['domain']
+            return response
+
+    def user_friends(self):
+        if self.user_info():
+            params = {
+                'v': '5.101',
+                'access_token': SERVICE_TOKEN,
+                'user_id': self.user_info()['response'][0]['id']
+            }
+            response = requests.get('https://api.vk.com/method/friends.get', params=params).json()
+            friends_list = set(response['response']['items'])
+            return friends_list
+        else:
+            return None
+
+
+def common_friends(set1, set2):
+    common = list(set1 & set2)
+    return common
+
+
+def userclass(userlist):
+    newlist = []
+    for i in userlist:
+        newlist.append(User(i))
+    return newlist
+
+
+def main():
+    user_a, command, user_b = input('Введите id 2-х пользователей и комманду в виде аперанда: ').split()
+    if command == '&':
+        user1, user2 = User(user_a), User(user_b)
+        if user1.user_info():
+            print(f'Пользователь {user1.link} запретил доступ к своему списку друзей')
+        elif user2.user_info():
+            print(f'Пользователь {user2.link} запретил доступ к своему списку друзей')
+        else:
+            common_friend = common_friends(user1.user_friends(), user2.user_friends())
+            classlist = userclass(common_friend)
+            print('Список общих друзей: ')
+            for i in classlist:
+                i.user_info()
+                print('{} - {} {}'.format(i.link, i.first_name, i.last_name))
+    else:
+        print('Введена неверная команда')
+
+
+if __name__ == '__main__':
+    main()
